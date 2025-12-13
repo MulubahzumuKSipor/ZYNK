3e17fb75ebb1d0dbe28b589054ffa43ad51ad108
MulubahzumuKSipor <sip23001@byui.edu>
2025-12-13 13:26:45 +0000

    First Phase

--- src/app/ui/components/shared/footer.tsx
+++ src/app/ui/components/shared/footer.tsx
@@ -10,13 +10,13 @@ export default function Footer(){
                 </p>
                 <div className={styles.footerLinks}>
                     <a href="https://app.daily.dev/mulubahzumukemmehsipor" target="_blank"
-                    ><Image width={150} height={100} src="/dailydev.svg" alt="Daily Developer"
+                    ><Image width={150} height={100} src="/dailydev.svg" alt="Daily Developer" className={styles.img}
                     /></a>
                     <a href="https://www.linkedin.com/in/mulubahzumu-kemmeh-sipor-526a74197/" target="_blank"
-                    ><Image width={100} height={100} src="/linkedin.svg" alt="LinkedIn"
+                    ><Image width={100} height={100} src="/linkedin.svg" alt="LinkedIn" className={styles.img}
                     /></a>
                     <a href="https://github.com/MulubahzumuKSipor" target="_blank"
-                    ><Image width={100} height={100} src="/github.svg" alt="Github"
+                    ><Image width={100} height={100} src="/github.svg" alt="Github" className={styles.img}
                     /></a>
                 </div>
             </footer>
